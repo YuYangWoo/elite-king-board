6c67bd7100c953c72764d9cbcb6d6e0bfe563350
YangWoo Yu <yuyw0712@naver.com>
2022-02-22 16:00:39 +0900

    feat(#15): Retrofit Setting

---
+++ android/app/src/main/java/com/example/elitekingboard/data/model/request/SignUpRequest.kt
@@ -0,0 +1,7 @@
+package com.example.elitekingboard.data.model.request
+
+data class SignUpRequest(
+        val nickname: String,
+        val password: String,
+        val email: String
+)
---
+++ android/app/src/main/java/com/example/elitekingboard/data/model/response/SignUpResponse.kt
@@ -0,0 +1,5 @@
+package com.example.elitekingboard.data.model.response
+
+class SignUpResponse(
+
+)
